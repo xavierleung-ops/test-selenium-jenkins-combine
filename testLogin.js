0ff13880ef4dda398306c7118da11e16bc87bfeb
xavierleung-ops <xavier.leung@okg.com>
2024-07-30 15:54:40 +0800

    Update testLogin.js with additional chrome boot arg

--- testLogin.js
+++ testLogin.js
@@ -1,11 +1,18 @@
 const { Builder, By, Key, until } = require('selenium-webdriver');
 const assert = require('assert');
 
+// Added options for not found DevToolsActivePort file
+const options = new chrome.Options()
+
 async function loginTest() {
   let driver;
   try {
+    console.log('Init options for browser')
+    options.addArguments('--disable-dev-shm-usage')
+    options.addArguments('--no-sandbox')
+    options.addArguments('--headless')
     console.log('Starting the browser...');
-    let builder = new Builder().forBrowser('chrome');
+    let builder = new Builder().forBrowser('chrome').setChromeOptions(options);
     console.log("Building chrome");
     driver = await builder.build();
 
