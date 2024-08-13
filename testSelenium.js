0a1c47ffa1af134ae2ddc2840020facc1b4a1183
xavierleung-ops <xavier.leung@okg.com>
2024-08-14 01:34:39 +0800

    Update testSelenium.js

--- testSelenium.js
+++ testSelenium.js
@@ -4,8 +4,8 @@ const assert = require('assert');
 const options = new chrome.Options();
 
 // Configure options for Chrome
-options.addArguments('--disable-dev-shm-usage');
-options.addArguments('--no-sandbox');
+// options.addArguments('--disable-dev-shm-usage');
+// options.addArguments('--no-sandbox');
 options.addArguments('--headless');
 
 describe('Google Search Tests', () => {
